30f731b9059c950938f2cc362e12bbc04d6f202a
cSScripter <parksolprojects@gmail.com>
2025-10-23 16:32:00 +0300

    fade in header image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,23 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Header() {
-    return (
-          <header className="flex flex-col md:flex-row justify-center items-center">
-            <Image
-              src="/homepage/ecoplasmatechnologies.avif"
-              alt="EcoPlasma Technologies Logo"
-              width={470}
-              height={390}
-            />
-            </header>
-    )
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <header className="flex flex-col md:flex-row justify-center items-center">
+      <Image
+        src="/homepage/ecoplasmatechnologies.avif"
+        alt="EcoPlasma Technologies Logo"
+        width={470}
+        height={390}
+        onLoadingComplete={() => setLoaded(true)}
+        className={`transition-opacity duration-700 ease-in-out ${
+          loaded ? "opacity-100" : "opacity-0"
+        }`}
+        priority
+      />
+    </header>
+  );
 }
